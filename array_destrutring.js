cc1fd4d745a2777771bc2cc83e1df6b55917a681
Muhammad Saif Ur Rehman <120719748+saifee403@users.noreply.github.com>
2023-11-13 12:05:16 +0000

    array destructring practice

--- array_destrutring.js
+++ array_destrutring.js
@@ -4,3 +4,8 @@ console.log(person1)
 console.log(person2)
 console.log(person3)
 console.log(person4)
+let personal = ["abdullah","abdul","zia"]
+let [p1,p2,p3] = personal;
+console.log(p1)
+console.log(p2)
+console.log(p3)
