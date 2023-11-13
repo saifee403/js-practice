d1c2e01d434e38be6f52cbf0f7b01641e90cc58b
Muhammad Saif Ur Rehman <120719748+saifee403@users.noreply.github.com>
2023-11-13 06:24:35 +0000

    arrow function

---
+++ arrow_function.js
@@ -0,0 +1,9 @@
+const addTwoNum = (num1,num2)=>{
+ return num1 + num2
+}
+console.log(addTwoNum(55, 67))
+
+//implecit return
+
+const addTwo = (num1,num2) => num1 + num2
+console.log(addTwo(22,57));
--- object.js
+++ object.js
@@ -2,3 +2,17 @@ function cartItem(...num){
     return num
 }
 console.log(cartItem(23,24,25,26))
+
+
+
+const obj = {
+    fName: "saif",
+    lName: "Rehman",
+    age: 23,
+    education:"BSCS"
+}
+
+function getDetails(anyObj){
+    return (`my name is ${anyObj.fName} ${anyObj.lName} my age is ${anyObj.age}`)
+}
+ console.log(getDetails(obj))
