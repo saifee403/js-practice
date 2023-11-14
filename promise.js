f5f79ea815e831e3c382602069a79290900d005a
Muhammad Saif Ur Rehman <120719748+saifee403@users.noreply.github.com>
2023-11-14 13:39:34 +0000

    promise practice

--- promise.js
+++ promise.js
@@ -8,12 +8,61 @@
 // })
 
 //2nd method
-function prom(complete){
-    return new Promise(function(resolve, reject){
-        if(!complete){
-            resolve("failed")
-        }else{
-            reject("success")
-        }
+// function prom(complete){
+//     return new Promise(function(resolve, reject){
+//         if(complete){
+//             resolve("success")
+//         }else{
+//             reject("failed")
+//         }
+//     })
+// }
+// let onFulfuilment = (result) =>{
+//     console.log(result)
+// }
+// let onRejection = (err) =>{
+//     console.log(err)
+// }
+// prom(true).then(onFulfuilment)
+// prom(true).catch(onRejection)
+
+// function prom(complete){
+//     return new Promise(function(resolve, reject){
+//         console.log("fetching dat, please wait............");
+//         setTimeout(()=>{
+//             if(complete){
+//                 resolve("success")
+//             }else{
+//                 reject("failed")
+//             }
+//         },2000)
+//     })
+// }
+// let onFulfuilment = (result) =>{
+//     console.log(result)
+// }
+// let onRejection = (err) =>{
+//     console.log(err)
+// }
+// prom(false).then(onFulfuilment).catch(onRejection)
+
+//5th method
+function prom(a, b) {
+    return new Promise(function (resolve, reject) {
+        console.log("fetching dat, please wait............");
+        let c = a/b
+        setTimeout(() => {
+            if (a,b) {
+                resolve(`your answer is: ${c}`)
+            } else {
+                reject("Failed")
+            }
+        }, 2000)
     })
 }
+
+prom(5,2).then((result) => {
+    console.log(result)
+}).catch((err) => {
+    console.log(err)
+})
